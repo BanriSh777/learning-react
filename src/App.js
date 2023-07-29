64aba0de655e27caa99c10e1e3396edf05eff7f1
BanriSh777 <56825696+BanriSh777@users.noreply.github.com>
2023-07-29 13:17:41 +0530

    creating, nesting, styling

--- src/App.js
+++ src/App.js
@@ -2,10 +2,21 @@ import React from 'react';
 import './style.css';
 
 export default function App() {
+  return <Main />;
+}
+
+function Main() {
+  return <main class="main-view"></main>;
+}
+
+function Header() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <header>
+      <h2>My React App</h2>
+    </header>
   );
+}
+
+function Button() {
+  return <button class="my-button">My Button</button>;
 }
