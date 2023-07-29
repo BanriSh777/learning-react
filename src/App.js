511ff6f8ece18bcff020a8d4d2ce95df6d13a4d4
BanriSh777 <56825696+BanriSh777@users.noreply.github.com>
2023-07-29 17:47:40 +0530

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -5,11 +5,61 @@ export default function App() {
   return <Main className="main-view" />;
 }
 
+var isLoggedIn = true;
+
 function Main() {
   return (
     <main>
-      <Name />
+      <Session />
     </main>
+  );
+}
+
+function Session() {
+  let content;
+  if (isLoggedIn) {
+    console.log('Logged in - Name will be displayed');
+    content = <Name />;
+  } else {
+    console.log('Not Logged in - Btn will be displayed to log in');
+    content = <LoginButton />;
+  }
+  return (
+    <div className={isLoggedIn ? 'authenticated' : 'unauthenticated'}>
+      {content}
+    </div>
+  );
+}
+
+function LoginButton() {
+  console.log('Button displayed');
+  let count = 0;
+  let counter;
+
+  let login = () => {
+    isLoggedIn = true;
+    console.log('Logged in');
+    counterFn();
+  };
+
+  let counterFn = () => {
+    counter = 'You pressed no of times: ';
+    count = count + 1;
+
+    if (count) {
+      counter = counter + count;
+      console.log(counter);
+    } else {
+      counter = '';
+    }
+  };
+
+  return (
+    <>
+      <button className="login-button" onClick={login}>
+        Login Button
+      </button>
+    </>
   );
 }
 
@@ -22,7 +72,7 @@ function Header() {
 }
 
 function Button() {
-  return <button class="my-button">My Button</button>;
+  return <button className="my-button">My Button</button>;
 }
 
 function Name() {
@@ -30,5 +80,9 @@ function Name() {
   const lname = 'addipalli';
   let nameColor = 'red';
   let name = fname.toUpperCase() + ' ' + lname.toUpperCase();
-  return <span style={{ color: nameColor }}>{name}</span>;
+  return (
+    <span style={{ color: nameColor, fontWeight: 'bold', fontSize: '40px' }}>
+      {name}
+    </span>
+  );
 }
